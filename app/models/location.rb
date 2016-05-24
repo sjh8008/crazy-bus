53668129fb8c6a13be604775a9929f2180485eec
sarah <sjh741@yahoo.com>
2016-05-23 19:37:27 -0500

    Set up location Scaffold, set up model

---
+++ app/models/location.rb
@@ -0,0 +1,8 @@
+class Location < ActiveRecord::Base
+  geocoded_by :full_street_address
+  after_validation :geocode #auto-fetches coords from geocoder gem
+
+  def full_street_address
+    "#(street_address), #(city), GA"
+  end
+end
